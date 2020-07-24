25e67d71b90212898bd59f897fa5ecfe8605fa23
Tony Kim <hjkmines@gmail.com>
2020-07-24 11:17:44 -0600

    finished

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -5,9 +5,22 @@ class KidsController < ApplicationController
     render json: { kids: @kids }
   end
 
+  def new 
+    @kid = Kid.new 
+    @teams = Team.all 
+  end 
+
   def create
     @kid = Kid.create(kid_params)
   
+    if @kid.valid? 
+      @kid.save 
+      redirect_to kid_path(@kid)
+    else 
+      render :new 
+      @kid.errors 
+    end 
+
     render json: { kid: @kid }
   end
 
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,6 @@
 class Kid < ApplicationRecord
+
+  validates :age, inclusion: {in: 7..14}, precense: true, numericality: {only_integer: true}  
+  validates :username, uniqueness: true, length: {in: 7..25}, precense: true  
+
 end
